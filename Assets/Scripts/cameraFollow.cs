dd2d6f4c54f1732d5d6e28eca25e96b657f1a6d9
Sam Anderson <samel.2710@gmail.com>
2015-08-22 21:10:10 +0100

    Basic camera follow
    Camera follows a target object.
    Debug switches for zooming in/out

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+
+	public Transform target;
+	private float watchZ = -10f;
+	private float watchX = 10f;
+	private float watchY = 10f;
+
+	void LateUpdate(){
+		if(target){
+			float watchX = target.GetComponent<Rigidbody>().transform.position.x;
+			float watchY = target.GetComponent<Rigidbody>().transform.position.y;
+			transform.position = new Vector3(watchX, watchY, watchZ);
+		}
+		
+		// Debug: Zoom in/out manually
+		if(Input.GetKeyDown("e")){
+			watchZ++;
+		}else if(Input.GetKeyDown("q")){
+			watchZ--;
+		}
+	}
+	
+	// TO DO: Dynamic resizing
+	void dynamicResize(){
+		
+	}
+}
