d6c77624412d436b365be28049506017063cc2a0
Sam Anderson <samel.2710@gmail.com>
2015-08-22 23:41:22 +0100

    Camera script updates
    zoomTo function allows smooth transitions to specified FoV

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -8,18 +8,16 @@ public class cameraFollow : MonoBehaviour {
 	private float watchX = 10f;
 	private float watchY = 10f;
 
+	public Camera thisCamera;
+	public float zoomSpeed = 20f;
+	public float minZoomFOV = 1f;
+	public float maxZoomFOV = 100f;
+	
 	void LateUpdate(){
 		if(target){
-			float watchX = target.GetComponent<Rigidbody>().transform.position.x;
-			float watchY = target.GetComponent<Rigidbody>().transform.position.y;
+			watchX = target.GetComponent<Rigidbody>().transform.position.x;
+			watchY = target.GetComponent<Rigidbody>().transform.position.y;
 			transform.position = new Vector3(watchX, watchY, watchZ);
-		}
-		
-		// Debug: Zoom in/out manually
-		if(Input.GetKeyDown("e")){
-			watchZ++;
-		}else if(Input.GetKeyDown("q")){
-			watchZ--;
 		}
 	}
 	
@@ -27,4 +25,57 @@ public class cameraFollow : MonoBehaviour {
 	void dynamicResize(){
 		
 	}
+
+	public void zoomTo(float zoomPoint){
+		float difference;
+		if(thisCamera.fieldOfView != zoomPoint){
+			if(zoomPoint>maxZoomFOV){
+				zoomPoint = maxZoomFOV;
+			}else if(zoomPoint<minZoomFOV){
+				zoomPoint = minZoomFOV;
+			}
+			difference = Mathf.Abs(zoomPoint-thisCamera.fieldOfView);
+			for(int i=0; i<10;i++){
+				if(thisCamera.fieldOfView < zoomPoint){
+					thisCamera.fieldOfView += difference/10;
+				}else{
+					thisCamera.fieldOfView -= difference/10;
+				}
+			}
+		}
+	}
+	
+	public void zoomTo(float zoomPoint, int smoothing){
+		float difference;
+		if(thisCamera.fieldOfView != zoomPoint){
+			if(zoomPoint>maxZoomFOV){
+				zoomPoint = maxZoomFOV;
+			}else if(zoomPoint<minZoomFOV){
+				zoomPoint = minZoomFOV;
+			}
+			difference = Mathf.Abs(zoomPoint-thisCamera.fieldOfView);
+				for(int i=0; i<smoothing;i++){
+					if(thisCamera.fieldOfView < zoomPoint){
+						thisCamera.fieldOfView += difference/smoothing;
+					}else{
+						thisCamera.fieldOfView -= difference/smoothing;
+					}
+				}
+			}
+	}
+	
+	public void ZoomIn()
+	{
+		if (thisCamera.fieldOfView > minZoomFOV){
+			thisCamera.fieldOfView -= zoomSpeed/8;
+		}
+	}
+	
+	public void ZoomOut()
+	{
+		if (thisCamera.fieldOfView < maxZoomFOV){
+			thisCamera.fieldOfView += zoomSpeed/8;
+		}
+	}
+	
 }
