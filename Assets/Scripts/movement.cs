9c50f3cf5ca9562e44873a5bfa73fbf8159a2a37
Sam Anderson <samel.2710@gmail.com>
2015-08-22 15:16:02 +0100

    VERY basic movement top down movement script with variable speed

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float speed = 0f;
+	private float moveX = 0f;
+	private float moveY = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveX = 0f;
+		moveY = 0f;		
+		
+		if(Input.GetKey("a")){
+			moveX = -1;
+		}
+		if(Input.GetKey("d")){
+			moveX = 1;
+		}
+		if(Input.GetKey("w")){
+			moveY = 1;
+		} 
+		if(Input.GetKey("s")){
+			moveY = -1;
+		}
+		
+		 transform.Translate(moveX*speed, moveY*speed, 0);
+		
+	}
+}
