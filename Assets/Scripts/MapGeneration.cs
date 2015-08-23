33249bb8c92567327527ca91a2bd518dc6f1c369
Jack Harrison <jack@jhrrsn.com>
2015-08-23 15:17:50 +0100

    cleaned up

---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+	[Range(0, 100)]
+	public float settlementDensity;
+
+	[Range(0, 100)]
+	public float caveDensity;
+
+	public int gridDensity;
+	public int mapSize;
+	public GameObject settlement;
+	public GameObject wolfCave;
+	public GameObject map;
+
+	private int grid;
+	private int[,] populatedGrid;
+
+	void Start () {
+		grid = mapSize / gridDensity;
+		populatedGrid = new int[grid, grid];
+		float sumDensities = settlementDensity + caveDensity;
+		settlementDensity /= sumDensities;
+		caveDensity /= sumDensities;
+		PopulateGrid ();
+		InstantiateObjects ();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void PopulateGrid () {
+		for (int i = 0; i < grid; i++) {
+			for (int j = 0; j < grid; j++) {
+				if (i % grid == 0 || j % grid == 0) {
+					populatedGrid[i,j] = 0;
+				} else if (Random.value < caveDensity) {
+					populatedGrid[i,j] = 2;
+				} else if (Random.value < settlementDensity) {
+					populatedGrid[i,j] = 1;
+				} else {
+					populatedGrid[i,j] = 0;
+				}
+			}
+		}
+	}
+
+	void InstantiateObjects () {
+		for (int i = 0; i < grid; i++) {
+			for (int j = 0; j < grid; j++) {
+				// Settlements
+				if (populatedGrid[i,j] == 1) {
+					int settlementSize = Random.Range(3, 8) * 10;
+					int settlementRotation = Random.Range(0, 359);
+					Vector2 settlementPosition = new Vector2((i * gridDensity)-mapSize/2f, (j * gridDensity)-mapSize/2f);
+					float xOffset = Random.Range(-gridDensity/5f, gridDensity/5f);
+					float yOffset = Random.Range(-gridDensity/5f, gridDensity/5f);
+					Vector2 settlementOffset = new Vector2(xOffset, yOffset);
+					GameObject newSettlement = (GameObject) Instantiate (settlement, settlementPosition + settlementOffset, transform.rotation);
+					newSettlement.transform.parent = map.transform;
+					SettlementGeneration sGenr = newSettlement.GetComponent<SettlementGeneration>();
+					sGenr.BroadcastMessage("SetSettlementRotation", settlementRotation);
+					sGenr.BroadcastMessage("SetSettlementSize", settlementSize);
+					sGenr.BroadcastMessage("SpawnBuildings");
+				} else if (populatedGrid[i,j] == 2) {
+					Quaternion caveRotation = Quaternion.AngleAxis (Random.Range(0, 359), Vector3.forward);
+					Vector2 cavePosition = new Vector2((i * gridDensity)-mapSize/2f, (j * gridDensity)-mapSize/2f);
+					float xOffset = Random.Range(-gridDensity/2.5f, gridDensity/2.5f);
+					float yOffset = Random.Range(-gridDensity/2.5f, gridDensity/2.5f);
+					Vector2 caveOffset = new Vector2(xOffset, yOffset);
+					GameObject newCave = (GameObject) Instantiate (wolfCave, cavePosition + caveOffset, caveRotation);
+					newCave.transform.parent = map.transform;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SettlementGeneration.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SettlementGeneration : MonoBehaviour {
+
+	public int roadLength;
+	public GameObject[] buildingObjects;
+	public GameObject road;
+	public int[] buildingSizes;
+	public int buildingSpacerSize;
+	public int buildingGapSize;
+	public int roadOffset;
+	public float roadRotation;
+
+	[Range (10, 90)]
+	public float buildingDensity;
+	
+
+//	void Start () {
+//		SpawnBuildings ();
+//	}
+
+	void SpawnBuildings () {
+		InstantiateBuildings (GenerateRowBuildings (), true);
+		InstantiateBuildings (GenerateRowBuildings (), false);
+
+		GameObject newRoad = (GameObject) Instantiate(road, transform.position, transform.rotation);
+		Vector3 roadSize = newRoad.transform.localScale;
+		roadSize.x = roadLength * 1.2f;
+		newRoad.transform.localScale = roadSize;
+
+//		Vector2 roadPositon = newRoad.transform.position;
+//		roadPositon.x += roadLength / 2f;
+//		newRoad.transform.position = roadPositon;
+
+		newRoad.transform.parent = this.transform;
+
+		transform.Rotate(0, 0, roadRotation);
+//		transform.RotateAround(
+		
+	}
+
+	List<int> GenerateRowBuildings () {
+		// Create list to store the buildings.
+		List<int> buildings = new List<int>();
+
+		// Variable to track the total length of the row as we add buildings/space.
+		int rowLength = 0;
+
+		// Bool to ensure we don't have successful spacers.
+		bool previousSpacer = false;
+
+		while (true) {
+			if (Random.value * 100f < buildingDensity || previousSpacer) {
+				// Pick one of the building sizes at random from the supplied list.
+				int building = Random.Range(0, buildingSizes.Length);
+				int buildingLength = (int) buildingSizes.GetValue(building);
+				if ((rowLength + buildingLength + buildingSpacerSize) > roadLength) {
+					break;
+				} else {
+					// Add selected building to array.
+					buildings.Add(building);
+					rowLength += buildingLength+buildingSpacerSize;
+					previousSpacer = false;
+				}
+			} else {
+				if ((rowLength + buildingGapSize) > roadLength) {
+					break;
+				} else {
+					// Add spacer ID (99) to array.
+					buildings.Add(99);
+					rowLength += buildingGapSize;
+					previousSpacer = true;
+				}
+			}
+		}
+
+		return buildings;
+	}
+
+	void InstantiateBuildings(List<int> buildingList, bool sideA) {
+		Vector2 cursorPosition = transform.position;
+		cursorPosition.x -= roadLength / 2f;
+
+		foreach (int b in buildingList) {
+			if (b < 99) {
+				int buildingSize = buildingSizes[b];
+
+				// Move cursor along.
+				Vector2 newCursor = cursorPosition;
+				newCursor.x += buildingSize/2.0f;
+				cursorPosition = newCursor;
+
+				float yValue;
+
+				if (sideA) yValue = transform.position.y + buildingSize/2.0f + roadOffset;
+				else yValue = transform.position.y -roadOffset - buildingSize/2.0f;
+
+				GameObject newBuilding = (GameObject) Instantiate(buildingObjects[b], new Vector2(cursorPosition.x, yValue), transform.rotation);
+				newBuilding.transform.parent = this.transform;
+
+				// Move cursor along.
+				newCursor = cursorPosition;
+				newCursor.x += buildingSize/2.0f;
+				cursorPosition = newCursor;
+			} else {
+				Vector2 newCursor = cursorPosition;
+				newCursor.x += buildingGapSize;
+				cursorPosition = newCursor;
+			}
+		}
+	}
+
+	void SetSettlementRotation (float rot) {
+		roadRotation = rot;
+	}
+
+	void SetSettlementSize (int size) {
+		roadLength = size;
+	}
+}
