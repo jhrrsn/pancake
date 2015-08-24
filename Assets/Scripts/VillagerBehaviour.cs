ca6f356eb007346bec7892c368000472aaff618a
Jack Harrison <jack@jhrrsn.com>
2015-08-24 13:35:30 +0100

    villagers have a sprite and turn to face direction of travel

--- Assets/Scripts/VillagerBehaviour.cs
+++ Assets/Scripts/VillagerBehaviour.cs
@@ -9,13 +9,30 @@ public class VillagerBehaviour : MonoBehaviour {
 	public LayerMask wolfLayer;
 	public bool flight = true;
 	public float lerpSpeed;
+	public Color [] hairColours;
 
 	private int health;	
 	private Rigidbody2D rb;
+	private Animator anim;
+	private bool running;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		GetComponent<SpriteRenderer> ().color = hairColours [Random.Range (0, hairColours.Length)];
 		health = maxHealth;
+		running = false;
+	}
+
+	void Update () {
+		// Animation
+		if (!running && rb.velocity.sqrMagnitude > 0) {
+			running = true;
+			anim.SetBool ("running", true);
+		} else if (running && rb.velocity.sqrMagnitude < 1f) {
+			running = false;
+			anim.SetBool ("running", false);
+		}
 	}
 
 	void FixedUpdate () {
@@ -33,11 +50,15 @@ public class VillagerBehaviour : MonoBehaviour {
 		wolfVector /= wolfCount;
 		wolfVector = wolfVector.normalized;
 
-		Debug.DrawRay (transform.position, wolfVector);
-
 		if (flight) {
+			// Set fleeing velocity.
 			Vector2 fleeVector = -wolfVector * runSpeed;
 			rb.velocity = Vector2.Lerp (rb.velocity, fleeVector, lerpSpeed * Time.deltaTime);
+
+			// Set look rotation.
+			Vector2 look = rb.velocity.normalized;
+			var angle = Mathf.Atan2 (look.y, look.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
 		}
 	}
 
